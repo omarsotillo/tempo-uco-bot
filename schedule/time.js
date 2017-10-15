3e818346f10662efdb6a83a9fbee4f8d6be8144d
OMAR SOTILLO FRANCO <i32sofro@uco.es>
2017-10-15 11:49:05 +0200

    :pencil: Fixed problem

--- schedule/time.js
+++ schedule/time.js
@@ -2,11 +2,15 @@ module.exports = {
   getTime() {
     const d = new Date();
     let dateMinutes = d.getMinutes();
+    let dateHours = d.getHours();
     // Adding zero function
     if (dateMinutes < 10) {
       dateMinutes = `0${dateMinutes}`;
     }
+    if (dateHours < 10) {
+      dateHours = `0${dateHours}`;
+    }
     console.log(`${d.getHours()}:${dateMinutes}`);
-    return `${d.getHours()}:${dateMinutes}`;
+    return `${dateHours}:${dateMinutes}`;
   },
 };
